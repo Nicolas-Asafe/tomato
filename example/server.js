6af305e8ae0b8a625a64e17246c6fd36d80d8d22
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-05-24 12:03:55 -0300

    feat: NewSyVAR added

--- example/server.js
+++ example/server.js
@@ -1,13 +1,7 @@
 import tomato from '../src/index.js'
 const api = new tomato.Group("users")
 
-const nome = (nome = "nome") => tomato.syvars.access(nome)
 
-tomato.syvars.setVar("nome",'nicolas')
-console.log(nome())
-
-tomato.syvars.setVar("nome","nicole")
-console.log(nome())
 
 //modules routers
 api.autoLoadRoutesFrom("./routers")
