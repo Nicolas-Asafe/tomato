4bbb33e079b1cd2902b3dd4e431007ce638e8c4b
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-05-26 14:36:59 -0300

    feat: syvars, newsyvarlocal

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import NewRegister from "./newRegister.js";
 import buildReponse from "./buildResponse.js";
 import { listVars,access,setVar } from "./system/syvars.js";
 
-const NewSyVAR = (name = "value",value="none") => {
+const NewSyvarLocal = (name = "value",value="none") => {
     setVar(name,value)
     return [
         ()=>access(name),
         (value)=>{setVar(name,value)}
     ]
 }
+const syvars = {access,setVar,listVars}
+
 const tomato = {
-  Group,Server,NewRegister,buildReponse,NewSyVAR,listVars
+  Group,Server,NewRegister,buildReponse,NewSyvarLocal,syvars
 }
 
 export default tomato
